79fe795d9e42e689f394a18d82cba4c9f36b2c6f
BaiYuli <cdwind@SAMSMINI.local>
2015-03-19 18:09:00 +0900

    Refactor the code. add wikipedia en -> zh . add ignore list

--- xwords.py
+++ xwords.py
@@ -5,47 +5,96 @@ import sys
 import urllib2
 import json
 
-files = []
-if len(sys.argv) > 1:
-	for file in sys.argv[1:]:
-		files.append(str(file))
-else:
-	print "Usage: xwords.py file1 file2 file3 ..."
- 
-text = ""
-for file in files:
-	f = open(file,"rU")
-	for line in f:
-		text += line
- 
-words = text.lower().split()
-wordcount = {}
-
-temp=""
-known_file = open("./db/known","rU")
-for line in known_file:
-    temp+=line
-
-known_words=temp.lower().split()
-
-
-for word in words:
-	if word.isalpha() and len(word)>2:
-		if word in wordcount:
-			wordcount[word] += 1
-		else:
-			wordcount[word] = 1
-		
-frequency =  sorted(wordcount,key=wordcount.get,reverse=True)
- 
 shanbay_api = 'https://api.shanbay.com/bdc/search/?word='
+wikipedia_api = 'http://en.wikipedia.org/w/api.php?action=query&prop=langlinks&continue&format=json&lllimit=500&&titles='
 
-for word in frequency:
-    if word in known_words:
-        break;
-    res = urllib2.urlopen(shanbay_api+word)
-    js = json.load(res)
-    if js['status_code']==0 :
+ignore_file = './db/ignore'
+known_file = './db/known'
+prep_file = './db/prep'
+pron_file = './db/pron'
+
+ignore_known = True
+ignore_prep = True
+ignore_pron = True
+
+min_word_len = 3
+max_word_len = 15
+
+def isword(word):
+    if not word.isalpha() :
+        return False
+    if len(word)<min_word_len or len(word)>max_word_len :
+        return False
+    return True
+
+def search_shanbay(word):
+    js = json.load(urllib2.urlopen(shanbay_api+word))
+    pronun = ""
+    defin = ""
+    if js['status_code'] == 0:
         pronun = js['data']['pronunciation'].encode('utf-8')
         defin = js['data']['definition'].encode('utf-8').replace('\n','\t')
-        print wordcount[word],word,'/'+pronun+'/',defin
+    return pronun+defin
+
+def search_wikipedia(word):
+    wiki_res = urllib2.urlopen(wikipedia_api+word)
+    js = json.load(wiki_res)
+    if 'langlinks' in js['query']['pages'].values()[0].keys():    
+        for x in js['query']['pages'].values()[0]['langlinks'] :
+            if x['lang']=='zh':
+                return x['*']
+    '''
+    else:
+        return word.upper()+" does not exist on Wikipedia. "
+        '''
+
+def load_words(files):
+    text = ""
+    f = open(files,"rU")
+    for line in f :
+        text += line
+    words = text.lower().split()
+    for word in words :
+        if not isword(word) :
+            words.remove(word)
+    return words
+
+
+def load_ignore():
+    files = []
+    text = ""
+    files.append(ignore_file)
+    if ignore_known :
+        files.append(known_file)
+    if ignore_prep :
+        files.append(prep_file)
+    if ignore_pron :
+        files.append(pron_file)
+
+    for file in files :
+        f = open(file,"rU")
+        for line in f:
+            text += line
+    words = text.lower().split()
+    return words
+
+def sort_words(words):
+    wordcount = {}
+    for word in words :
+        if word in wordcount :
+            wordcount[word] +=1
+        else :
+            wordcount[word] = 1
+    fre = sorted(wordcount,key=wordcount.get,reverse=True)
+    return fre
+
+if __name__ == '__main__':
+
+    twords = load_words(sys.argv[1])
+    ignore = load_ignore()
+    words = sort_words(twords) 
+
+    for word in words :
+        if word not in ignore :
+            print word,search_shanbay(word)
+
